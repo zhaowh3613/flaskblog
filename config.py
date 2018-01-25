6a02930112aea1bf173f5860836a97a920cb0511
zhaowh3613 <zhaowh3613@outlook.com>
2018-01-25 14:00:49 +0800

    add program construct

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'hard to guess string'
+    # os.environ.get('SECRET_ KEY') or
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')\
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI =\
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    # os.environ.get('DEV_DATABASE_URL') or
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI =\
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+# os.environ.get('TEST_DATABASE_URL') or
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI =\
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    # os.environ.get('DATABASE_URL') or
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
