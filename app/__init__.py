cc4058c799b47ef959ba5f0723428c521e74ee3e
zhaowh3613 <zhaowh3613@outlook.com>
2018-01-25 18:04:16 +0800

    app blueprint init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+from config import config
 
-app = Flask(__name__)
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     return app
