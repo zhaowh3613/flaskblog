63126b4ec3932451542dc78f72d52396dd873658
wenhuizhao <zhaowh3613@outlook.com>
2018-01-28 14:00:54 +0800

    clear waring FlaskForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Email, Length
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     emial = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     username = StringField('what is your name?', validators=[Required()])
     password = StringField('Password', validators=[Required(), Length(1, 64)])
